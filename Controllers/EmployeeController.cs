f9c92243af8c9d1041e3d99d31a2708a8e7a8442
unknown <User@DDTKILPC0341.ddtkonstract.com>
2018-11-15 16:51:24 +0800

    Change layout, added employee view details, customize autocomplete for search box

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -18,8 +18,24 @@ namespace hr_201_file.Controllers
             JobCategory jobcategory = db.JobCategories.Single(x => x.JobCategoryId == emp.JobCategoryId);
 
             ViewBag.jobCategory = jobcategory.Description;
+            ViewBag.fileCategory = db.FileCategories.ToList();
 
-            return View(db.Employees.First());
+            return View(db.Employees.Single(e => e.EmpNo == 13109));
+        }
+
+        public ActionResult Employee_Records(int folder_id, int EmpNo)
+        {
+            List<FileContents> files = db.FileContents.Where(x => x.File201_FileCategory_id == folder_id && x.employee_number == EmpNo).ToList();
+            return View(files);
+        }
+
+        public JsonResult autocomplete(string term)
+        {
+            List<string> Names = db.FileContents.Where(e => e.employee_name.Contains(term))
+                                    .OrderBy(y => y.employee_name)
+                                    .Select(x => x.employee_name).ToList();
+
+            return Json(Names, JsonRequestBehavior.AllowGet);
         }
 	}
 }
--- Scripts/Custom.js
+++ Scripts/Custom.js
@@ -8,12 +8,84 @@
     })
 }
 
-subNav = (e) => {
+subNav = (e, folder_id, emp_no) => {
     let removeActive = document.querySelectorAll('.sub-nav-link');
-
+    let btnUpload = document.querySelector('.upload-link');
+    
     removeActive.forEach(al => {
         al.classList.remove('active');
     })
-
+    
     e.target.classList.add('active');
+    
+    btnUpload.href = `/File/New_Record?folder_id=${folder_id}&search=${emp_no}`;
+    
+    $.get('/Employee/Employee_Records', {
+        folder_id : folder_id,
+        EmpNo : emp_no
+    }, data => {
+        console.log(data);
+        document.querySelector('.profile__content').innerHTML = data;
+    });
 }
+
+loadEmployeeDetails = () => {
+    document.querySelector('.fa-address-card').parentElement.classList.add('active-nav');
+}
+
+toggleUpload = () => {
+    let btnSearch = document.querySelector('#btnSearch');
+    btnSearch.type = "submit";
+}
+
+showSuggestion = (e) => {
+    let suggestion = document.querySelector('.search__suggestion');
+    let txtSearch = document.querySelector('.search__bar--text');
+    let list = "";
+    let arr_list = [];
+    
+    if(txtSearch.value.length > 2) {
+        suggestion.hidden = false;
+    } else {
+        suggestion.hidden = true;
+    }
+    
+    txtSearch.onblur = () => {
+        suggestion.hidden = true;
+    }
+    
+    $.get('/Employee/autocomplete', {
+        term : e.target.value
+    }, data => {
+        
+        data.forEach(item => {
+            if(!NameExist(item)) {
+                list = list + `<li class="search__suggestion--item">${item}</li>`
+                arr_list.push(item);
+            }
+        });
+        
+        document.querySelector('.search__suggestion--list').innerHTML = list;
+    });
+    
+    NameExist = (name) => {
+        let isExist = false;
+        arr_list.forEach(arr_data => {
+            if(arr_data == name)
+                isExist = true;
+        });
+
+        return isExist;
+    }
+}
+
+set_DOM_on_page_load = () => {
+    let current_page = location.href;
+    let btnSearch = document.querySelector('#btnSearch');
+    
+    if(current_page.includes("New_Record")) {
+        btnSearch.type = "submit";
+    }
+}
+
+set_DOM_on_page_load();
